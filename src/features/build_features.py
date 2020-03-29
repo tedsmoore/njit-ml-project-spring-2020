a33be523dbb48e477643982eb0e8f14726f601e3
tedsmoore <tedsmoore@gmail.com>
2020-03-29 15:11:57 -0400

    added new label condition for future sma rise

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,56 +1,76 @@
 import pandas as pd
 from pyalgotrade import strategy
 from pyalgotrade.technical import ma, linreg
-from pyalgotrade.dataseries import SequenceDataSeries
 
 
 class BuildFeatures(strategy.BacktestingStrategy):
+    """
+    Construct indicators from various stock analysis techniques and generate labels for training data
+
+    Example usage:
+    b = BuildFeatures()
+    X, y = b.run()
+    """
     def __init__(self, feed, instrument):
         strategy.BacktestingStrategy.__init__(self, feed)
-        self.__instrument = instrument
-        self.__sma = ma.SMA(feed[instrument].getAdjCloseDataSeries(), 15)
-        self.__ema = ma.EMA(feed[instrument].getAdjCloseDataSeries(), 15)
+        self.instrument = instrument
+        self.sma = ma.SMA(feed[instrument].getAdjCloseDataSeries(), 15)
+        self.ema = ma.EMA(feed[instrument].getAdjCloseDataSeries(), 15)
         self.adj_close = feed[instrument].getAdjCloseDataSeries()
         self.adj_close_slope = linreg.Slope(feed[instrument].getAdjCloseDataSeries(), 15)
-        self.cols = ["Date", "Ticker", "Adj Close", "Adj Close - SMA15", "Adj Close - EMA15"]
+        self.cols = ["Date", "Ticker", "Adj Close", "SMA15", "Adj Close - SMA15", "Adj Close - EMA15"]
         self.features = pd.DataFrame(columns=self.cols)
+
+
+    ### Signals (Features)
+
+    def adj_close_diff_sma(self, bar):
+        try:
+            return bar.getAdjClose() - self.sma[-1]
+        except TypeError:
+            return None
+
+    def adj_close_diff_ema(self, bar):
+        try:
+            return bar.getAdjClose() - self.ema[-1]
+        except TypeError:
+            return None
+
 
     ### Buy/Sell Conditions (Labels)
 
     def stock_will_rise(self, days=1):
         return [1 if val > 0 else 0 for val in self._future_chg_adj_close(days)]
 
+    def sma_will_rise(self, days=15, sma_days=15):
+        future_change = [None for _ in range(sma_days)]
+        future_change.extend([1 if val > 0 else 0 for val in self._future_chg_sma(days)])
+        return future_change
+
     def _future_chg_adj_close(self, days=1):
         future_change = [self.adj_close[idx + days] - close for idx, close
                          in enumerate(self.adj_close[:len(self.adj_close) - days])]
         return future_change
 
+    def _future_chg_sma(self, days=15, sma_days=15):
+        future_change = [self.sma[idx + sma_days + days] - self.sma[idx + sma_days] for idx, _
+                              in enumerate(self.sma[sma_days:len(self.sma) - days])]
+        return future_change
+
     def stock_up_by_pct(self, bars, pct, num_days):
-        today = bars[self.__instrument].getAdj
+        today = bars[self.instrument].getAdj
 
-    ### Signals (Features)
-
-    def adj_close_diff_sma(self, bar):
-        try:
-            return bar.getAdjClose() - self.__sma[-1]
-        except TypeError:
-            return None
-
-    def adj_close_diff_ema(self, bar):
-        try:
-            return bar.getAdjClose() - self.__ema[-1]
-        except TypeError:
-            return None
 
     ### Overrides (do the dataset building)
 
     def onBars(self, bars):
-        bar = bars[self.__instrument]
+        bar = bars[self.instrument]
         # build up this list of signal features
         technicals = pd.DataFrame([[
             bar.getDateTime(),
-            self.__instrument,
+            self.instrument,
             bar.getAdjClose(),
+            self.sma[-1],
             self.adj_close_diff_sma(bar),
             self.adj_close_diff_ema(bar)
         ]],
@@ -58,8 +78,7 @@ class BuildFeatures(strategy.BacktestingStrategy):
         self.features = self.features.append(technicals, ignore_index=True)
 
     def onFinish(self, bars):
-        self.labels = pd.DataFrame(self.stock_will_rise(), columns=["Label"])
-        # to use this as a features set
-        # return self.featuers, self.labels
+        self.labels = pd.DataFrame(self.sma_will_rise(), columns=["Label"])
         dataset = pd.concat([self.features, self.labels], axis=1)
         print(dataset.tail())
+        return self.featuers, self.labels
