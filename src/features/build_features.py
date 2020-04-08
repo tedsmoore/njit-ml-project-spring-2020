128b5119a427f996911bb5cc249e3b67558cf633
tedsmoore <tedsmoore@gmail.com>
2020-04-08 12:02:22 -0400

    add a verbose argument to observe trades

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,15 +1,15 @@
-import pandas as pd
+
 import numpy as np
 import ta
 
 
-class AlgoTrading:
+class StockTechnicals:
     """
-    A class for creating technical indicators as features and labels from various strategies for training
+    A class for creating technical indicators of stocks as features and labels from various strategies for training
     Machine Learning models
     :param data: A pandas dataframe of a daily stock ticker data
     Usage:
-    a = AlgoTrading(data)
+    a = StockTechnicals(data)
     X = a.features
     y = a.price_will_rise()
     """
@@ -21,9 +21,21 @@ class AlgoTrading:
             open="Open",
             high="High",
             low="Low",
-            close="Adj Close",
+            close="Close",
             volume="Volume"
-        ).drop(columns=["Open", "High", "Low", "Close", "Adj Close", "Volume"])
+        ).drop(
+            columns=[
+                "Date",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Adj Close",
+                "Volume",
+                'trend_psar_up',
+                'trend_psar_down',
+                'trend_psar']
+        ).dropna()
 
     # Can add more derived metrics here
     # e.g. slopes, cross-overs
@@ -31,11 +43,11 @@ class AlgoTrading:
     # Possible Strategy labels
 
     def price_will_rise(self, days=1):
-        return np.array(self.data.apply(self._price_will_rise, days=days, axis=1))
+        return np.array(self.features.apply(self._price_will_rise, days=days, axis=1))
 
     def _price_will_rise(self, row, days=1):
         try:
-            if self.data.loc[row.name + days, 'Adj Close'] > row['Adj Close']:
+            if self.data.loc[row.name + days, 'Adj Close'] > self.data.loc[row.name, 'Adj Close']:
                 return 1
             return 0
         except KeyError:
@@ -43,16 +55,16 @@ class AlgoTrading:
 
     def future_sma_higher_than_current_price(self, days=7):
         # This metric is the most promising as far as an investment strategy
-        return np.array(self.data.apply(self._future_sma_v_price, days=days, axis=1))
+        return np.array(self.features.apply(self._future_sma_v_price, days=days, axis=1))
 
     def _future_sma_v_price(self, row, days=7):
         try:
-            future_sma = np.mean([self.data.loc[row.name + days + 1, 'Adj Close'] for days in range(days)])
+            future_sma = np.mean([self.data.loc[row.name + day + 1, 'Adj Close'] for day in range(days)])
         except KeyError:
             return None
 
         try:
-            if future_sma > row['Adj Close']:
+            if future_sma > self.data.loc[row.name, 'Adj Close']:
                 return 1
             return 0
         except KeyError:
