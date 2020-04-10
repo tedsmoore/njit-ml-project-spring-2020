bed934fe5d29f21ca8cfa26093656c61a8426502
tedsmoore <tedsmoore@gmail.com>
2020-04-10 11:23:54 -0400

    engineering features, tweak charts

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import ta
+from sklearn.linear_model import LinearRegression
 
 
 class StockTechnicals:
@@ -36,9 +37,38 @@ class StockTechnicals:
                 'trend_psar_down',
                 'trend_psar']
         ).dropna()
+        self.data['price_less_trend_sma_fast'] = self.data.apply(
+            self._price_less_sma_fast, axis=1)
+        self.data['trend_sma_fast_slope'] = self.data.apply(
+            self._slope, key='trend_sma_fast', days=3, axis=1)
+        self.data['trend_sma_fast_2nd_der'] = self.data.apply(
+            self._slope, key='trend_sma_fast_slope', days=3, axis=1)
+        self.features['price_less_trend_sma_fast'] = self.data['price_less_trend_sma_fast']
+        self.features['trend_sma_fast_slope'] = self.data['trend_sma_fast_slope']
+        self.features['trend_sma_fast_2nd_der'] = self.data['trend_sma_fast_2nd_der']
 
     # Can add more derived metrics here
     # e.g. slopes, cross-overs
+    def price_less_sma_fast(self):
+        return np.array(self.features['trend_sma_fast'].apply(self._price_less_sma_fast))
+
+    def _price_less_sma_fast(self, row):
+        return self.data.loc[row.name, 'Open'] - row['trend_sma_fast']
+
+    def _slope(self, row, key, days):
+        if row.name < days:
+            return None
+        try:
+            y = np.array([self.data.loc[row.name - days + i, key] for i in range(days)])
+        except ValueError:
+            return None
+        if np.isnan(y).any():
+            return None
+        x = np.array([i for i in range(days)]).reshape((-1, 1))
+        lr = LinearRegression()
+        lr.fit(x, y)
+        return lr.coef_[0]
+
 
     # Possible Strategy labels
 
@@ -59,12 +89,12 @@ class StockTechnicals:
 
     def _future_sma_v_price(self, row, days=7):
         try:
-            future_sma = np.mean([self.data.loc[row.name + day + 1, 'Adj Close'] for day in range(days)])
+            future_sma = np.mean([self.data.loc[row.name + day + 1, 'Close'] for day in range(days)])
         except KeyError:
             return None
 
         try:
-            if future_sma > self.data.loc[row.name, 'Adj Close']:
+            if future_sma > self.data.loc[row.name, 'Open']:
                 return 1
             return 0
         except KeyError:
